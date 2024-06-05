42eb21e3c2fdd332fe3f1fba7e405d022864fa9c
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-05 16:14:23 +0800

    move Calendar into separate component

--- appointments-frontend/src/App.jsx
+++ appointments-frontend/src/App.jsx
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import config from "./config";
+import Calendar from "./components/Calendar";
 
 function App() {
-  const [appointments, setAppointments] = useState(null);
-
-  useEffect(() => {
-    fetch(`${config.API_URL}/appointments/`) // Adjust the URL to your Django endpoint
-      .then((response) => response.json())
-      .then((data) => setAppointments(data))
-      .catch((error) => console.error("Error:", error));
-  }, []);
-
   return (
-    <div>
-      <h1>Data from Django</h1>
-      {appointments ? (
-        <pre>{JSON.stringify(appointments, null, 2)}</pre>
-      ) : (
-        "Loading..."
-      )}
-    </div>
+    <>
+      <Calendar />
+    </>
   );
 }
 
---
+++ appointments-frontend/src/components/Calendar.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import config from "../config";
+
+function Calendar() {
+  const [appointments, setAppointments] = useState(null);
+
+  useEffect(() => {
+    fetch(`${config.API_URL}/appointments/`) // Adjust the URL to your Django endpoint
+      .then((response) => response.json())
+      .then((data) => setAppointments(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Django</h1>
+      {appointments ? (
+        <pre>{JSON.stringify(appointments, null, 2)}</pre>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
+}
+export default Calendar;
