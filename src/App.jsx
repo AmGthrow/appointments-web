a7aea57ad49395d44cfc33f22bb4c0c5452a71b7
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-06 21:50:23 +0800

    add style for mui elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import Calendar from "./components/Calendar";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <Calendar />
-    </>
+    </ThemeProvider>
   );
 }
 
